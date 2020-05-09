fcd7c79019857ed743a8bb647cf06f5a3d7e76ad
Dariusz Gorski <Dariusz.Gorski@levo.com.au>
2020-05-09 20:46:39 +1000

    CRUD application in .NET Core using the CQRS + MediatR pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -31,7 +33,8 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-                services.AddControllers();
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
